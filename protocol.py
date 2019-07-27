a6bbff0cac634943759981af3a85fd6e85326f86
Lucas Suhyuk Park <nicenick15@gmail.com>
2019-07-27 11:51:45 +0900

    Add braille support

---
+++ protocol.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+import braille as b
+
+def get_clipboard():
+    root = Tk()
+    root.withdraw()
+    result = None
+    try:
+        result = root.clipboard_get()
+    except:
+        raise LookupError
+    return result
+
